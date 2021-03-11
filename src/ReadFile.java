7aba00318d91ae6616ee39a3267712cba559074f
Aleister <detuongtuongthat@gmail.com>
2021-03-11 22:38:22 +0700

    Init

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -14,39 +14,47 @@ public class ReadFile {
 
     public static void main(String[] args) {
 
+
+        System.out.print("Dir to Parse: ");
+        Scanner input = new Scanner(System.in);
+        String dir = input.nextLine();
+
+
         while(true){
+
             List<String> data = new ArrayList<>();
 
-            System.out.println("File to Parse: ");
+            System.out.print("File to Parse: ");
 
             Scanner scanner = new Scanner(System.in);
             String nextLine = scanner.nextLine();
 
             try {
-                File myObj = new File("C:\\Users\\Aleister\\OneDrive\\Document\\Year 3\\Sem 2\\Speech Processing\\work\\" + nextLine + "\\" + nextLine + ".txt");
+                File myObj = new File(dir + "\\" + nextLine + "\\" + nextLine + ".txt");
                 Scanner myReader = new Scanner(myObj);
                 while (myReader.hasNextLine()) {
                     String line = myReader.nextLine();
                     List<String> word = Arrays.asList(line.split(" "));
                     data.addAll(word);
-                    System.out.println(word);
                 }
+                System.out.println("Done read");
                 myReader.close();
             } catch (FileNotFoundException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
+                System.out.println("Can't find file to read.");
             }
 
             try {
-                FileWriter myWriter = new FileWriter("C:\\Users\\Aleister\\OneDrive\\Document\\Year 3\\Sem 2\\Speech Processing\\work\\"+ nextLine + "\\data" + nextLine + ".txt");
+                FileWriter myWriter = new FileWriter(dir + "\\" + nextLine + "\\data" + nextLine + ".txt");
                 for(String str : data) {
                     myWriter.write(str + "\n");
                 }
+                System.out.println("Done Write");
                 myWriter.close();
             } catch (IOException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
+                System.out.println("Can't write file.");
             }
+
+            System.out.println();
         }
         }
 }
